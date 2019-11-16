73efafa2e080def911e36358913419250b68c903
Bindzi <dawidbindis@gmail.com>
2019-11-16 21:04:39 +0100

    ocr

---
+++ app/src/main/java/com/example/application/TessOcr.java
@@ -0,0 +1,92 @@
+package com.example.application;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+public class TessOcr {
+    private final TessBaseAPI mTess;
+
+    public TessOcr(Context context, String lang){
+        mTess = new TessBaseAPI();
+        boolean fileExistFlag = false;
+
+        AssetManager assetManager = context.getAssets();
+        String dstPathDir = "/tesseract/tessdata/";
+        String srcFile = "eng.traineddata";
+        dstPathDir = context.getFilesDir() + dstPathDir;
+        String dstInitPathDir = context.getFilesDir() + "/tesseract";
+        String dstPathFile = dstPathDir + srcFile;
+
+        InputStream inFile = null;
+        FileOutputStream outFile = null;
+
+        try {
+            inFile = assetManager.open(srcFile);
+
+            File f = new File(dstPathDir);
+
+            if (!f.exists()) {
+                if (!f.mkdirs()) {
+                    Toast.makeText(context, srcFile + " can't be created.", Toast.LENGTH_SHORT).show();
+                }
+                outFile = new FileOutputStream(new File(dstPathFile));
+            } else {
+                fileExistFlag = true;
+            }
+
+        } catch (Exception ex) {
+            Log.e(TAG, ex.getMessage());
+
+        } finally {
+
+            if (fileExistFlag) {
+                try {
+                    if (inFile != null) inFile.close();
+                    mTess.init(dstInitPathDir, lang);
+                    return;
+
+                } catch (Exception ex) {
+                    Log.e(TAG, ex.getMessage());
+                }
+            }
+
+            if (inFile != null && outFile != null) {
+                try {
+                    //copy file
+                    byte[] buf = new byte[1024];
+                    int len;
+                    while ((len = inFile.read(buf)) != -1) {
+                        outFile.write(buf, 0, len);
+                    }
+                    inFile.close();
+                    outFile.close();
+                    mTess.init(dstInitPathDir, lang);
+                } catch (Exception ex) {
+                    Log.e(TAG, ex.getMessage());
+                }
+            } else {
+                Toast.makeText(context, srcFile + " can't be read.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public String getOCRResult(Bitmap bitmap) {
+        mTess.setImage(bitmap);
+        return mTess.getUTF8Text();
+    }
+
+    public void onDestroy() {
+        if (mTess != null) mTess.end();
+    }
+}
